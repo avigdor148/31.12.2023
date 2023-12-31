845728d92ad8d79215cfb53022d11c9143e31735
fullstack290623 <fullstack290623@gmail.com>
2023-12-31 20:35:53 +0200

    .

--- manip/dom_man1x.js
+++ manip/dom_man1x.js
@@ -10,15 +10,27 @@ function clear_txt() {
 // add row to the users table with the input type data from the user
 function add_row(idv, user_namev, emailv, countryv) {
     const new_row = users_table.insertRow(-1)
+
     const cell_id = new_row.insertCell(0)
     const cell_name = new_row.insertCell(1)
     const cell_email = new_row.insertCell(2)
     const cell_country = new_row.insertCell(3)
+    const cell_flag = new_row.insertCell(4)
 
     cell_id.innerHTML = idv
     cell_name.innerHTML = user_namev
     cell_email.innerHTML = emailv
     cell_country.innerHTML = countryv
+
+    switch (countryv.toUpperCase()) {
+        case 'ISRAEL':
+            cell_flag.innerHTML = '<img src="images/israel.png" style="width:50px;" />'
+            break;
+        case 'USA':
+            cell_flag.innerHTML = '<img src="images/usa.jpg" style="width:50px;"/>'
+            break;
+        // add country
+    }
 
     clear_txt()
 }
