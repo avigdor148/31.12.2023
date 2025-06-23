59e496a94a329383a5fce1501b763ce6eb4193a3
avigdor148 <asg14826@gmail.com>
2025-06-23 21:51:37 +0300

    y

--- manip/dom_man1x.js
+++ manip/dom_man1x.js
@@ -15,17 +15,28 @@ function add_row(idv, user_namev, emailv, countryv) {
     const cell_name = new_row.insertCell(1)
     const cell_email = new_row.insertCell(2)
     const cell_country = new_row.insertCell(3)
+    const cell_flag = new_row.insertCell(4)
 
     cell_id.innerHTML = idv
     cell_name.innerHTML = user_namev
     cell_email.innerHTML = emailv
     cell_country.innerHTML = countryv
 
+    switch (countryv.toUpperCase()) {
+        case 'ISRAEL':
+            cell_flag.innerHTML = '<img src="images/israel.png" style="width:50px;" />'
+            break;
+        case 'USA':
+            cell_flag.innerHTML = '<img src="images/usa.jpg" style="width:50px;"/>'
+            break;
+        // add country
+    }
+
     clear_txt()
 }
 // add button click
-
-function addBtn(){
+debugger
+function add_click(){
     const idv = id.value
     const user_namev = user_name.value
     const emailv = email.value
